a493032a402befe291287c4e1771d340f678ffde
Jose Pablo Brenes Alfaro <josepablo1997@hotmail.com>
2019-05-28 10:32:10 -0600

    feat: adding provider into router

--- src/app/provider/provider-routing.module.ts
+++ src/app/provider/provider-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { ProviderRootComponent } from './provider-root/provider-root.component';
+import { ProviderComponent } from './provider/provider.component';
 
-const ROUTES: Routes = [{ path: "provider", component: ProviderRootComponent }];
+const ROUTES: Routes = [
+  { path: 'provider', component: ProviderRootComponent },
+  {path: 'mainProvider', component: ProviderComponent},
+];
 
 @NgModule({
   declarations: [],
