e707b400fa1ff810bacb3ab6a243213ef2acd865
Justin Alberto Castillo Valladares <justincastillovalladares@gmail.com>
2019-05-28 10:20:16 -0600

    router: router configured

---
+++ src/app/provider/provider-root/provider-root.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-provider-root',
+  templateUrl: './provider-root.component.html',
+  styleUrls: ['./provider-root.component.scss']
+})
+export class ProviderRootComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/provider/provider.module.ts
+++ src/app/provider/provider.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProviderRootComponent } from './provider-root/provider-root.component';
+import { ProviderRoutingModule } from './provider-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [ProviderRootComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    ProviderRootComponent,
+    ProviderRoutingModule
+  ],
+  exports: [ProviderRootComponent]
 })
 export class ProviderModule { }
