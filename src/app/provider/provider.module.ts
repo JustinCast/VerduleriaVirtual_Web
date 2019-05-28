e6f4a991dc028a37952af3437f8dd44a93e53589
Jose Pablo Brenes Alfaro <josepablo1997@hotmail.com>
2019-05-28 10:21:29 -0600

    feat: adding component provider and login

--- src/app/provider/provider.module.ts
+++ src/app/provider/provider.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { ProviderComponent } from './provider/provider.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, ProviderComponent],
   imports: [
     CommonModule
   ]
