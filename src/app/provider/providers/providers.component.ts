f30cb884facd7e3b2bee6951af6b74167b3d3599
Jose Pablo Brenes Alfaro <josepablo1997@hotmail.com>
2019-05-29 22:35:33 -0600

    feat: changing provider component

--- src/app/provider/providers/providers.component.ts
+++ src/app/provider/providers/providers.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import LocationPicker from "location-picker";
 import { ProviderService } from 'src/app/services/provider.service';
 @Component({
   selector: 'app-providers',
@@ -18,20 +17,6 @@ export class ProvidersComponent implements OnInit {
     this._ps.getProviders().subscribe(list => {
       this.listProviders = list.slice();
       this.loading = false;
-      this.fillMap();
     }, err =>{ console.log(err); this.loading = false;});
-  }
-
-  // Fill location maps providers
-  fillMap(){
-    this.listProviders.forEach( provider => {
-      let lp = new LocationPicker(`${provider.id}`, {
-        setCurrentPosition: false, // You can omit this, defaults to true
-      }, {
-        zoom: 15,
-        draggable: false
-      });
-      lp.setLocation(provider.lat, provider.lon);
-    });
   }
 }
