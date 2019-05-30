d22b7eb12348d944b42c018b8b62518ed42ff912
Justin Alberto Castillo Valladares <justincastillovalladares@gmail.com>
2019-05-29 22:08:48 -0600

    feat: purchases report gotten

---
+++ src/app/models/PurchaseReport.ts
@@ -0,0 +1,7 @@
+export class PurchaseReport {
+  constructor(
+    public product_name?: string,
+    public total_product?: number,
+    public _date?: Date
+  ) {}
+}
--- src/app/services/provider.service.ts
+++ src/app/services/provider.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { MatSnackBar } from "@angular/material";
+import { environment } from "src/environments/environment";
+import { Observable } from "rxjs";
+import { PurchaseReport } from "../models/PurchaseReport";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ProviderService {
+  constructor(private _http: HttpClient, private _snackBar: MatSnackBar) {}
 
-  constructor() { }
+  getPurchasesReport(id_provider: number, initial_date: string, final_date: string): Observable<Array<PurchaseReport>> {
+    return this._http.get<Array<PurchaseReport>>(
+      `${environment.SERVER_BASE_URL}getPurchasesByProvider/${id_provider}/${initial_date}/${final_date}`
+    );
+  }
+  
+  handleError(err: HttpErrorResponse) {
+    if (err.error instanceof Error) {
+      // Error del lado del cliente
+      console.log("An error occurred:", err.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // Error del lado del backend
+      console.log(
+        `Backend returned code ${err.status}, body was: ${JSON.stringify(
+          err.error
+        )}`
+      );
+    }
+  }
 }
