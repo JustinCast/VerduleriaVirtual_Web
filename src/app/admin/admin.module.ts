0f74fdf0f016a1d9a7f588080cde6f711cdb564f
Luis <luigoncal98@gmail.com>
2019-06-03 22:32:07 -0600

    minimal changes in the nav-bar

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home/home.component';
 import { AdminRootComponent } from './admin-root/admin-root.component';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AddProductComponent } from './add-product/add-product.component';
@@ -10,8 +9,8 @@ import { SharedModule } from '../shared/shared.module';
   declarations: [
     AdminRootComponent,
     AddProductComponent,
-    AdminRootComponent,
-    HomeComponent],
+    AdminRootComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule,
