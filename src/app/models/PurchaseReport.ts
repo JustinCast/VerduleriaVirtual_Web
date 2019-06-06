6d9522a57dc2359a9102f54b956c0c3ff44698d0
Justin Alberto Castillo Valladares <justincastillovalladares@gmail.com>
2019-06-06 13:32:49 -0600

    feat: show more data on purchases dialog result

--- src/app/models/PurchaseReport.ts
+++ src/app/models/PurchaseReport.ts
@@ -2,6 +2,8 @@ export class PurchaseReport {
   constructor(
     public product_name?: string,
     public _total_product?: number,
+    public price?: number,
+    public total_price?: number,
     public _date?: Date
   ) {}
 }
