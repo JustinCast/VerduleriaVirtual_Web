d551196b54f8f0192a9677e5f52491d5d5ee6ed6
Jose Pablo Brenes Alfaro <josepablo1997@hotmail.com>
2019-06-06 19:06:10 -0600

    feat: minimal changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     JSON.parse(localStorage.getItem('actual_user')).id === 'admin' ? this.admin = true : this.provider = true;
   }
+  //
   signOut(){
     this.loginService.isLogin = true;
   }
